1db6a97836c7182031308f79eb378f94af761ebd
Marcela Lima <marcelalima.df@gmail.com>
2023-01-23 15:22:23 -0300

    config eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+    root: true,
+    env: {
+    browser: true,
+    node: true,
+    },
+    extends: [
+    'plugin:vue/essential', 'eslint:recommended'
+    ],
+    rules: {
+    'vue/multi-word-component-names': 'off',
+    'no-debugger': 'off',
+    'no-unused-vars': 'off',
+    },
+    }
